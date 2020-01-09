3308972c7b9b6065473ed8a914d9c35c87765516
Xi Chen <kevinchen@me.com>
2020-01-09 16:07:27 +0100

    Initial Commit

---
+++ ModernNetwork/ModernNetwork.h
@@ -0,0 +1,19 @@
+//
+//  ModernNetwork.h
+//  ModernNetwork
+//
+//  Created by Xi Chen on 09.01.20.
+//  Copyright © 2020 Xi Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModernNetwork.
+FOUNDATION_EXPORT double ModernNetworkVersionNumber;
+
+//! Project version string for ModernNetwork.
+FOUNDATION_EXPORT const unsigned char ModernNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModernNetwork/PublicHeader.h>
+
+
---
+++ ModernNetworkTests/ModernNetworkTests.swift
@@ -0,0 +1,34 @@
+//
+//  ModernNetworkTests.swift
+//  ModernNetworkTests
+//
+//  Created by Xi Chen on 09.01.20.
+//  Copyright © 2020 Xi Chen. All rights reserved.
+//
+
+import XCTest
+@testable import ModernNetwork
+
+class ModernNetworkTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

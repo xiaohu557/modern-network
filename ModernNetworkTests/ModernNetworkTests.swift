4779b2c842233d27e344ece77000c80c554623a6
Xi Chen <kevinchen@me.com>
2020-01-16 14:21:22 +0100

    Add a test

--- ModernNetworkTests/ModernNetworkTests.swift
+++ ModernNetworkTests/ModernNetworkTests.swift
@@ -10,19 +10,32 @@ import XCTest
 @testable import ModernNetwork
 
 class ModernNetworkTests: XCTestCase {
-    let githubService = ServiceProvider<GitHubService>()
+    let dataProvider = GithubDataProvider()
 
     override func setUp() {
-        
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testFetchHottestRepos() {
+        let e = expectation(description: "Load data from network")
+        e.expectedFulfillmentCount = 2
+
+        dataProvider.hottestRepos.bind { value in
+            if value.count == 2 {
+                e.fulfill()
+            }
+        }
+        dataProvider.error.bind { error in
+            if error == nil {
+                e.fulfill()
+            }
+        }
+        dataProvider.fetchData()
+
+        wait(for: [e], timeout: 5.0)
     }
 
     func testPerformanceExample() {
